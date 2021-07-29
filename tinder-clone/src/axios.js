4de6b88b3cbdeb6f5cc88676081863d15aceb1bc
shyamlal98 <shyaml.cse16@gmail.com>
2021-07-29 23:10:27 +0530

    fix

--- tinder-clone/src/axios.js
+++ tinder-clone/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const instance = axios.create({
-    baseURL: 'http://127.0.0.1:8001',
-    timeout: 1000,
+    baseURL: 'https://tinder-back-ends.herokuapp.com',
+    timeout: 3000,
     headers: {'X-Custom-Header': 'Tinder Clone'}
   });
 export default instance;
