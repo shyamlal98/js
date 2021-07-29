27521703c30d03884793401fb0abb6787393242a
shyamlal98 <shyaml.cse16@gmail.com>
2021-07-29 23:45:40 +0530

    deployed on firebase react app

--- tinder-clone/src/axios.js
+++ tinder-clone/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const instance = axios.create({
     baseURL: 'https://tinder-back-ends.herokuapp.com',
-    timeout: 1000,
+    timeout: 3000,
     headers: {'X-Custom-Header': 'Tinder Clone'}
   });
 export default instance;
